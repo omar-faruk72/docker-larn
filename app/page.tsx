c5729dc2f47e1c7de47f742e5f00d83cb76183a5
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-05-09 15:54:00 +0600

    added an ci.yml file setup whit docker

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <h1>kdak</h1>
-   </div>
+    <div>
+      <h1>kdak</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab corporis
+        laborum, asperiores inventore perspiciatis ullam incidunt debitis ipsa.
+        Earum veniam, magnam adipisci porro laudantium dignissimos autem ipsa
+        repellat. Magnam, distinctio.
+      </p>
+    </div>
   );
 }
